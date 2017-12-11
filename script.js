9a5b7d58cbf8a0affbc71a24f260421f6ab87d3a
wmsiddiqui <wmsiddiqui.6@gmail.com>
2017-12-10 18:18:21 -0600

    update

---
+++ script.js
@@ -0,0 +1,69 @@
+window.onscroll = function() {
+	"use strict";
+	var dolphin = document.getElementById("dolphin");
+	var rectDolphin = dolphin.getBoundingClientRect();
+	var positionDolphin = (parseInt(rectDolphin.top, 10) * -1) + 300;
+	dolphin.style.left =  positionDolphin + "px";
+
+	var whale = document.getElementById("whale");
+	var rectWhale = whale.getBoundingClientRect();
+	var positionWhale = (parseInt(rectWhale.top, 10) * -1) + 200;
+	whale.style.left =  positionWhale + "px";
+
+	var turtle = document.getElementById("turtle");
+	var rectTurtle = turtle.getBoundingClientRect();
+	var positionTurtle = (parseInt(rectTurtle.top, 10)) + 300;
+	turtle.style.left =  positionTurtle + "px";
+};
+
+$("#bycatchingLink").click(function(e) { 
+	"use strict";
+	e.preventDefault();
+    $('html, body').animate({
+		scrollTop: $("#what-is-anchor").offset().top
+    }, 1000);
+});
+
+$("#impactsLink").click(function(e) { 
+	"use strict";
+	e.preventDefault();
+    $('html, body').animate({
+		scrollTop: $("#impacts-anchor").offset().top
+    }, 1000);
+});
+
+$("#solutionsLink").click(function(e) { 
+	"use strict";
+	e.preventDefault();
+    $('html, body').animate({
+		scrollTop: $("#solutions-anchor").offset().top
+    }, 1000);
+});
+
+$("#takeActionLink").click(function(e) { 
+	"use strict";
+	e.preventDefault();
+    $('html, body').animate({
+		scrollTop: $("#take-action-anchor").offset().top
+    }, 1000);
+});
+
+$("#topOfPage").click(function(e) { 
+	"use strict";
+	e.preventDefault();
+    $('html, body').animate({
+		scrollTop: 0
+    }, 1000);
+});
+
+$(document).on("scroll", function() {
+	"use strict";
+	if($(document).scrollTop()>100) {
+		$("#navigation").removeClass("navigationLarge").addClass("navigationMini");
+		$("#logo").removeClass("logoLarge", {duration:400}).addClass("logoMini", {duration:400});
+	} else {
+		$("#navigation").removeClass("navigationMini").addClass("navigationLarge");
+		$("#logo").removeClass("logoMini", {duration:400}).addClass("logoLarge", {duration:400});
+	}
+	
+});
